b117b582ca3dbc7d899851e8368f21341ea5bac5
reddy <reddy@192.168.0.14>
2022-07-30 22:36:31 -0400

    uploading poly

---
+++ Polymorphism/src/MOL/Addition.java
@@ -0,0 +1,21 @@
+package MOL;
+
+public class Addition {
+	public int a;
+	public double b;
+	public double c;
+	
+	/*public Addition(int a, int b, int c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		
+		System.out.println("addition");
+	}*/
+    public double  Add1(int a, double b) {
+	 return a+b;
+    }
+    public double Add1(int a, double b, double c) {
+    	return a+b+c;
+    }
+}
---
+++ Polymorphism/src/MOL/Mol_T.java
@@ -0,0 +1,16 @@
+package MOL;
+
+public class Mol_T {
+
+	public static void main(String[] args) {
+		Addition hint= new Addition();
+	 hint.Add1(10,20.9);
+	 hint.Add1(10,20.9,30.9);
+	 
+	 System.out.println("add"+ hint.Add1(10, 20.9));
+	 System.out.println("add"+ hint.Add1(10, 20.9, 30.9));
+	 
+	 
+}
+
+}
